36645c14dad4167cf32ac0ee4461940d1843f76b
Lane Yan <lane.yan@splashtop.com>
2017-12-21 13:36:17 +0800

    refine test job framework

--- src/main/main.go
+++ src/main/main.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
+//list test items
+const (
+	Basics = iota
+	Functions
+)
+
 func main() {
-	fff := 5
-	fmt.Println(fff)
-	fmt.Println("hello world!")
+	item := Functions
+	fmt.Printf("current test item: %v", item)
+
+	switch item {
+	case Basics:
+		runTestBasics()
+	case Functions:
+		runTestFunctions()
+	default:
+		fmt.Println("an unknown item to test")
+	}
 }
